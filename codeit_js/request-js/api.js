d9a6b56afd8a08a6814763563fbe5cf40135d3c9
kmk0531 <kmk7072301@gmail.com>
2025-01-29 02:54:38 +0900

    modified:   codeit_js/request-js/api.js 	modified:   codeit_js/request-js/main.js

--- codeit_js/request-js/api.js
+++ codeit_js/request-js/api.js
@@ -1,17 +1,21 @@
 export async function getColorSurveys(params = {}){
     const url = new URL("https://learn.codeit.kr/api/color-surveys");
+    //append query parameters to the url
     Object.keys(params).forEach((key) =>
         url.searchParams.append(key, params[key])
     );
+    //Send a GET request to the API
     const res = await fetch(url);
 
-    if (!res.ok){
+    if (!res.ok){ //checks vaild HTTP status code (200 ~299)
         throw new Error('데이터를 불러오는데 실패했습니다.');
     }
+    //Parse the JSON response
     const data = await res.json();
-    return data;
+    return data; //return the parsed data
 }
 
+// Function to get details of a specific color survey by its ID
 export async function getColorSurvey(id){
     const res = await fetch(`https://learn.codeit.kr/api/color-surveys/${id}`);
     if (!res.ok){
--- codeit_js/request-js/main.js
+++ codeit_js/request-js/main.js
@@ -1,11 +1,15 @@
 import {getColorSurveys, getColorSurvey, createColorSurvey} from './api.js';
 
+
+// Fetch a list of color surveys with pagination (offset and limit)
 const data = await getColorSurveys({offset: 20, limit: 20});
 console.log(data);
 
+// Fetch details of a specific color survey by its ID
 const data2 = await getColorSurvey(10);
 console.log(data2);
 
+// Define a new color survey object to create
 const surveyData = {
     mbti: 'ENFJ',
     colorCode: '#ABCD00',
@@ -15,6 +19,7 @@ const surveyData = {
 const data3 = await createColorSurvey(surveyData);
 console.log(data3);
 
+// Handle errors when fetching surveys with invalid parameters
 try{
     const data = await getColorSurveys({mbti: 'EEEE'});
     console.log(data);
