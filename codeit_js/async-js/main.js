5312bd5a990f78467d195a1360bbd2e1ab7731ff
kmk0531 <kmk7072301@gmail.com>
2025-02-13 12:43:30 +0900

    deleted:    CSS/bg.png 	deleted:    CSS/chainify.png 	deleted:    CSS/chainit-nano.png 	deleted:    CSS/chainit.png 	deleted:    CSS/index.html 	deleted:    CSS/style.css 	deleted:    Spring/hello-spring/.metadata/.plugins/org.eclipse.buildship.core/gradle/versions.json 	deleted:    Spring/hello-spring/build.gradle 	deleted:    Spring/hello-spring/src/main/java/hello/hello_spring/HelloSpringApplication.java 	deleted:    Spring/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java 	deleted:    Spring/hello-spring/src/main/resources/static/hello-static.html 	deleted:    Spring/hello-spring/src/main/resources/static/index.html 	deleted:    Spring/hello-spring/src/main/resources/templates/hello.html 	deleted:    codeit_js/async-js/asyncFunctions.js 	deleted:    codeit_js/async-js/callback.js 	deleted:    codeit_js/async-js/main.js     deleted:    codeit_js/async-js/package.json 	deleted:    codeit_js/module-js/calculator.mjs 	deleted:    codeit_js/module-js/main.mjs 	deleted:    codeit_js/module-js/package-lock.json 	deleted:    codeit_js/module-js/package.json 	deleted:    codeit_js/request-js/api.js 	deleted:    codeit_js/request-js/axiosApi.js 	deleted:    codeit_js/request-js/main.js 	deleted:    codeit_js/request-js/package.json

--- codeit_js/async-js/main.js
+++
@@ -1,6 +0,0 @@
-import { printEmployees } from "./asyncFunctions.js";
-
-printEmployees();
-
-console.log("Task 2");
-console.log("Task 3");
--- codeit_js/module-js/main.mjs
+++
@@ -1,4 +0,0 @@
-import {format} from 'date-fns';
-
-const dateString = format(new Date(2024, 1, 16), 'MM/dd/yyyy');
-console.log(dateString);
